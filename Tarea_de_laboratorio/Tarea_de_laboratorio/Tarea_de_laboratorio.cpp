8b1face485b492af1432bf78a5a57b9cf9583366
ItaloKanchaVilca <italo.kancha@ucsp.edu.pe>
2020-05-01 17:41:59 -0500

    4.26

---
+++ Tarea_de_laboratorio/Tarea_de_laboratorio/Tarea_de_laboratorio.cpp
@@ -0,0 +1,126 @@
+
+#include <iostream>
+using namespace std;
+
+//CAPITULO 4
+
+//4.26
+int main()
+{
+	int n;
+	cout << "ingrese el tamaño del cuadrado:  ";
+	cin >> n;
+	if (n != 1)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			cout << "*";
+		}
+		cout << "\r\n";
+		for (int i = 0; i < n - 2; i++)
+		{
+			cout << "*";
+			for (int j = 0; j < n - 2; j++)
+			{
+				cout << " ";
+			}
+			cout << "*";
+		}
+		cout << "\r\n";
+		for (int i = 0; i < n; i++)
+		{
+			cout << "*";
+		}
+	}
+	else
+	{
+		cout << "*";
+	}
+	return 0;
+
+
+//4.27
+
+
+
+//4.28
+
+
+
+//4.30
+
+
+
+
+
+
+
+//CAPITULO 5
+
+//5.11
+
+
+
+//5.15
+
+
+
+//5.19
+
+
+
+//5.29
+
+
+
+
+
+
+
+//CAPITULO 6 PARTE 1
+
+//6.12
+
+
+
+//6.18
+
+
+
+//6.20
+
+
+
+//6.25
+
+
+
+//6.29
+
+
+
+//6.30
+
+
+
+
+
+
+
+//CAPITULO 6 PARTE 2
+
+//6.36
+
+
+
+//6.37
+
+
+
+//6.39
+
+
+
+//6.41
+
+
